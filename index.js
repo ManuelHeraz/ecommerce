295df8ec8388e10ce56478099f232ca4d3b02e7e
Isaac Faraday <joseheraz04@outlook.com>
2023-04-01 18:17:50 -0600

    v1.05

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const jsonServer = requier("json-server");
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
